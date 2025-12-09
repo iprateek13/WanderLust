cf70c1c2c6b3bba367c6887bbd4114b615efa5ac
Prateek gupta <guptaji233001@gmail.com>
2025-12-09 15:00:53 +0530

    Styling of review Card

--- EzHomes/app.js
+++ EzHomes/app.js
@@ -80,7 +80,7 @@ app.get(
   "/listings/:id",
   wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listing/show.ejs", { listing });
   })
 );
